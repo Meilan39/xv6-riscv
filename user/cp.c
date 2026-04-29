391ac7918d066da3ef7928e534ce72083a44850a
Meilan39 <meilanmuto2005@gmail.com>
2026-04-29 20:47:41 +0900

    cp

---
+++ user/cp.c
@@ -0,0 +1,46 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "kernel/fcntl.h"
+#include "user/user.h"
+
+char buf[1024];
+
+void
+cp(int src, int dst)
+{
+  int n;
+
+  while((n = read(src, buf, sizeof(buf))) > 0) {
+    if (write(dst, buf, n) != n) {
+      fprintf(2, "cp: write error\n");
+      exit(1);
+    }
+  }
+  if(n < 0){
+    fprintf(2, "cp: read error\n");
+    exit(1);
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+  int src, dst;
+
+  if(argc != 3){
+    fprintf(2, "Usage: cp src dst\n");
+    exit(1);
+  }
+
+  if(((src = open(argv[1], O_RDONLY)) < 0) ||
+     ((dst = open(argv[2], O_CREATE | O_WRONLY)) < 0)){
+    fprintf(2, "cp: cannot open %s or %s\n", argv[1], argv[2]);
+    exit(1);
+  }
+ 
+  cp(src, dst);
+
+  close(src);
+  close(dst);
+  exit(0);
+}
